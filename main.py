a6502c740c41902d3b2ddc6fd22b98973e87ca8a
Robotz213 <nicholasazs213@gmail.com>
2024-07-21 21:08:38 -0400

    att

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ dotenv.load_dotenv()
 if __name__ == "__main__":
 
     debug = os.getenv("DEBUG").lower() in ("true", "1", "t", "y", "yes")
-    app.run(port=8080, debug=debug, host="0.0.0.0")
+    app.run(port=8282, debug=debug, host="0.0.0.0")
